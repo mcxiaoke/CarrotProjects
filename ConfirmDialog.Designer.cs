ffa564149f3e247b80e25ae0e2cbcceded811546
mcxiaoke <mcxiaoke@live.com>
2022-05-21 18:27:01 +0800

    add confirm dialog

---
+++ ConfirmDialog.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace GenshinNotifier {
+    partial class ConfirmDialog {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.ButtonA = new System.Windows.Forms.Button();
+            this.ButtonB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonA
+            // 
+            this.ButtonA.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.ButtonA.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ButtonA.Location = new System.Drawing.Point(86, 21);
+            this.ButtonA.Margin = new System.Windows.Forms.Padding(12);
+            this.ButtonA.Name = "ButtonA";
+            this.ButtonA.Size = new System.Drawing.Size(400, 70);
+            this.ButtonA.TabIndex = 0;
+            this.ButtonA.Text = "最小化到系统托盘";
+            this.ButtonA.UseVisualStyleBackColor = true;
+            this.ButtonA.Click += new System.EventHandler(this.ButtonA_Click);
+            // 
+            // ButtonB
+            // 
+            this.ButtonB.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.ButtonB.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ButtonB.Location = new System.Drawing.Point(86, 115);
+            this.ButtonB.Margin = new System.Windows.Forms.Padding(12);
+            this.ButtonB.Name = "ButtonB";
+            this.ButtonB.Size = new System.Drawing.Size(400, 70);
+            this.ButtonB.TabIndex = 1;
+            this.ButtonB.Text = "直接退出";
+            this.ButtonB.UseVisualStyleBackColor = true;
+            this.ButtonB.Click += new System.EventHandler(this.ButtonB_Click);
+            // 
+            // ConfirmDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 229);
+            this.Controls.Add(this.ButtonB);
+            this.Controls.Add(this.ButtonA);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ConfirmDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "退出确认";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonA;
+        private System.Windows.Forms.Button ButtonB;
+    }
+}
---
+++ ConfirmDialog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenshinNotifier {
+    public partial class ConfirmDialog : Form {
+        public ConfirmDialog() {
+            InitializeComponent();
+        }
+
+        private void ButtonA_Click(object sender, EventArgs e) {
+            Close();
+        }
+
+        private void ButtonB_Click(object sender, EventArgs e) {
+            Close();
+        }
+    }
+}
