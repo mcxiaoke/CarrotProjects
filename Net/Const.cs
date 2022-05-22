5316c5a4c639e88fd5c491fca4aed9617fc7c16d
mcxiaoke <mcxiaoke@live.com>
2022-05-22 23:33:51 +0800

    add some mihoyo bbs api salt

--- Net/Const.cs
+++ Net/Const.cs
@@ -11,12 +11,15 @@ namespace GenshinNotifier.Net {
         public const string BBS_API = "https://bbs-api.mihoyo.com";
         public const string TAKUMI_API = "https://api-takumi.mihoyo.com";
         public const string TAKUMI_RECORD_API = "https://api-takumi-record.mihoyo.com";
-        // 2.27.2 dailyNote worked must>2.11.0
-        public const string MHY_SALT_NEW = "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs";
+        // 2.27.2 dailyNote worked version must>2.11.0
+        public const string MHY_SALT_NEW = "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs";//new
         public const string MHY_VER_NEW = "2.27.2";
         public const string MHY_SIGN_ACT_ID = "e202009291139501";
-        //2.7.x sign worked
-        public const string MHY_SALT_OLD = "14bmu1mz0yuljprsfgpvjh3ju2ni468r";
+
+        // another salt 4a8knnbk5pbjqsrudp3dq484m9axoc5g // old
+        // another salt h8w582wxwgqvahcdkpvdhbh2w9casgfl // mweb old
+        //2.7.x sign worked for simple ds, sign api
+        public const string MHY_SALT_OLD = "14bmu1mz0yuljprsfgpvjh3ju2ni468r"; // mweb
         public const string MHY_VER_OLD = "2.7.0";
     };
 }
