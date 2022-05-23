5062da2fbdfd95d1cc1ca8b42bb4cc8111235667
mcxiaoke <mcxiaoke@live.com>
2022-05-23 21:51:04 +0800

    add apputils and cache store

---
+++ AppUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenshinNotifier.Properties;
+
+namespace GenshinNotifier {
+    public class AppUtils {
+
+        public static int ThreadId => System.Threading.Thread.CurrentThread.ManagedThreadId;
+
+        public const string ICON_FILE_NAME = "carrot_512.png";
+        public static readonly string IconFilePath = Path.Combine(Storage.UserDataFolder, "assets", ICON_FILE_NAME);
+        public static async Task CheckLocalAssets() {
+            await Task.Run(() => {
+                var assetsDir = Directory.GetParent(IconFilePath).FullName;
+                Storage.CheckOrCreateDir(assetsDir);
+                if (!File.Exists(IconFilePath)) {
+                    Resources.ImageCarrot512.Save(IconFilePath);
+                    Logger.Info($"CheckLocalAssets copied to {IconFilePath}");
+                }
+            });
+        }
+    }
+}
