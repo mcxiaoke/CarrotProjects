03b66f275deafae910fa1448dfeacdae759dcb73
mcxiaoke <mcxiaoke@live.com>
2022-05-30 15:28:03 +0800

    add carrot hash cli

---
+++ CarrotCommon/CarrotHash.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace CarrotCommon {
+
+    public static class CarrotHash {
+
+        public static string ToHexString(byte[] array, bool toUpper = false, bool grouping = false) {
+            var sb = new StringBuilder();
+            for (int i = 0; i < array.Length; i++) {
+                sb.Append($"{array[i]:x2}");
+                if ((i % 4) == 3 && grouping) {
+                    sb.Append(" ");
+                }
+            }
+            return toUpper ? sb.ToString().ToUpper() : sb.ToString();
+        }
+
+        public static string FileHash(string filepath, HashAlgorithm hasher, bool toUpper = false) {
+            var fInfo = new FileInfo(filepath);
+            using (FileStream stream = fInfo.Open(FileMode.Open)) {
+                try {
+                    // Create a fileStream for the file.
+                    // Be sure it's positioned to the beginning of the stream.
+                    stream.Position = 0;
+                    // Compute the hash of the fileStream.
+                    byte[] hashValue = hasher.ComputeHash(stream);
+                    return ToHexString(hashValue, toUpper);
+                } catch (IOException e) {
+                    Logger.Warning($"GetHash I/O Exception: {e.Message}");
+                } catch (UnauthorizedAccessException e) {
+                    Logger.Warning($"GetHash Access Exception: {e.Message}");
+                }
+            }
+            return null;
+        }
+
+
+        public static string FileMD5(string filepath, bool toUpper = false) {
+            using (HashAlgorithm hasher = MD5.Create()) {
+                return FileHash(filepath, hasher, toUpper);
+            }
+        }
+
+        public static string FileSHA1(string filepath, bool toUpper = false) {
+            using (HashAlgorithm hasher = SHA1.Create()) {
+                return FileHash(filepath, hasher, toUpper);
+            }
+        }
+
+        public static string FileSHA256(string filepath, bool toUpper = false) {
+            using (HashAlgorithm hasher = SHA256.Create()) {
+                return FileHash(filepath, hasher, toUpper);
+            }
+        }
+
+
+        public static string FileSHA512(string filepath, bool toUpper = false) {
+            using (HashAlgorithm hasher = SHA512.Create()) {
+                return FileHash(filepath, hasher, toUpper);
+            }
+        }
+
+        public static string GetHash(byte[] buffer, HashAlgorithm hasher, bool toUpper = false) {
+            try {
+                byte[] hashValue = hasher.ComputeHash(buffer);
+                return ToHexString(hashValue, toUpper);
+            } catch (IOException e) {
+                Logger.Warning($"GetHash I/O Exception: {e.Message}");
+            } catch (UnauthorizedAccessException e) {
+                Logger.Warning($"GetHash Access Exception: {e.Message}");
+            }
+            return null;
+        }
+
+        public static string GetHash(string text, HashAlgorithm hasher, bool toUpper = false) {
+            return GetHash(Encoding.UTF8.GetBytes(text), hasher, toUpper);
+        }
+
+
+        public static string GetMD5(string text, bool toUpper = false) {
+            using (HashAlgorithm hasher = MD5.Create()) {
+                return GetHash(text, hasher, toUpper);
+            }
+        }
+
+        public static string GetSHA1(string text, bool toUpper = false) {
+            using (HashAlgorithm hasher = SHA1.Create()) {
+                return GetHash(text, hasher, toUpper);
+            }
+        }
+
+        public static string GetSHA256(string text, bool toUpper = false) {
+            using (HashAlgorithm hasher = SHA256.Create()) {
+                return GetHash(text, hasher, toUpper);
+            }
+        }
+
+
+        public static string GetSHA512(string text, bool toUpper = false) {
+            using (HashAlgorithm hasher = SHA512.Create()) {
+                return GetHash(text, hasher, toUpper);
+            }
+        }
+
+
+
+    }
+}
---
+++ CarrotHash/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+using System.Globalization;
+using CarrotCommon;
+
+namespace CarrotHashCli {
+
+
+    public static class Extensions {
+        public static string Fixed(this string value, int totalWidth, char paddingChar = ' ') {
+            if (value is null)
+                return new string(paddingChar, totalWidth);
+
+            if (value.Length > totalWidth)
+                return value.Substring(0, totalWidth);
+            else
+                return value.PadRight(totalWidth, paddingChar);
+        }
+    }
+
+    public class Program {
+
+        private const long FILE_SIZE_1G = 1024 * 1024 * 1024;
+
+        private static void HashFile(string filepath) {
+            var file = new FileInfo(filepath);
+            Console.WriteLine();
+            Console.WriteLine("--------------------");
+            Console.WriteLine("{0,-10} {1}", "Path:".Fixed(10), file.FullName);
+            if (!file.Exists) {
+                Console.WriteLine("{0,-10} {1}\n", "Error:".Fixed(10), "Not exists or not regular file");
+                return;
+            }
+
+            // https://docs.microsoft.com/zh-cn/dotnet/standard/base-types/standard-numeric-format-strings
+            // https://docs.microsoft.com/zh-cn/dotnet/standard/base-types/composite-formatting
+            // https://docs.microsoft.com/zh-cn/dotnet/csharp/language-reference/tokens/interpolated
+            if (file.Length > FILE_SIZE_1G) {
+                decimal gbSize = Convert.ToDecimal(file.Length) / FILE_SIZE_1G;
+                Console.WriteLine("{0,-10} {1}", "Warning:".Fixed(10), $"File is too large, over {gbSize:F3}GB, " +
+                    $"computing hash may take a long time, contine? (yes/no)");
+                Console.Write("{0,-10}", "Choice: ");
+                var answer = Console.ReadLine();
+                if (answer.ToLower() != "yes") {
+                    Console.WriteLine("{0,-10} {1}", "Action:", "User abort.");
+                    return;
+                }
+            }
+
+            Console.WriteLine("{0,-10} {1}", "Name:", file.Name);
+            Console.WriteLine("{0,-10} {1}", "Size:", file.Length);
+            // https://stackoverflow.com/questions/114983
+            // https://code-maze.com/convert-datetime-to-iso-8601-string-csharp/
+            Console.WriteLine("{0,-10} {1}", "Creation:", file.CreationTime.ToString("s", CultureInfo.InvariantCulture));
+            Console.WriteLine("{0,-10} {1}", "Modified:", file.LastWriteTime.ToString("s", CultureInfo.InvariantCulture));
+            var timeStart = DateTime.Now;
+            var md5 = CarrotHash.FileMD5(filepath);
+            var sha1 = CarrotHash.FileSHA1(filepath);
+            var sha256 = CarrotHash.FileSHA256(filepath);
+            Console.WriteLine("{0,-10} {1}", "MD5:", md5);
+            Console.WriteLine("{0,-10} {1}", "SHA1:", sha1);
+            Console.WriteLine("{0,-10} {1}", "SHA256:", sha256);
+            var elapsed = DateTime.Now - timeStart;
+            if (elapsed.TotalSeconds > 10) {
+                Console.WriteLine("{0,-10} {1} seconds", "Elapsed:", Convert.ToInt32(elapsed.TotalSeconds));
+            }
+        }
+
+        public static void Main(string[] args) {
+            if (args.Length < 1 || string.IsNullOrWhiteSpace(args[0])) {
+                Console.WriteLine($"Usage: CarrotHashCli.exe myfile1.txt myfile2.exe myfile3.zip");
+                return;
+            }
+            args.ToList().FindAll(it => !string.IsNullOrWhiteSpace(it)).ForEach(it => HashFile(it));
+
+        }
+
+
+    }
+}
